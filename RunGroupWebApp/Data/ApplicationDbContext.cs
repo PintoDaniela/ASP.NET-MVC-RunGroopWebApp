70f7170f9eb40f5e3a8cb1f2d47aba169cf2d9c2
PintoDaniela <danielafpinto90@gmail.com>
2023-09-14 10:12:56 -0300

    login functionalty

--- RunGroupWebApp/Data/ApplicationDbContext.cs
+++ RunGroupWebApp/Data/ApplicationDbContext.cs
@@ -4,6 +4,10 @@ using RunGroupWebApp.Models;
 
 namespace RunGroupWebApp.Data
 {
+    //I want to use Identity, so I installed the Microsoft.AspNetCore.Identity.EntityFrameworkCore package (same version as EntityFrameworkCore)
+    //and changed DbContext to IdentityDbContext<AppUser>
+
+    //public class ApplicationDbContext : DbContext
     public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         //Constructor
